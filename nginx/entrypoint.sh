e9029119a5a1130d2154abdc634ae4271f51265b
Anatoly Bubenkov <bubenkoff@gmail.com>
2018-04-18 03:29:27 +0200

    remove CORS

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -215,22 +215,22 @@ cat << EOF >> /tmp/nginx.conf
         set \$http_origin "*";
       }
 
-      if (\$request_method = 'OPTIONS') {
-        add_header 'Access-Control-Allow-Origin' $SERVER_NAME always;
-        add_header 'Access-Control-Allow-Credentials' \$acac always;
-        add_header 'Access-Control-Allow-Methods' ${CORS_METHODS-'GET, POST, PUT, DELETE, HEAD, OPTIONS'} always;
-        add_header 'Access-Control-Allow-Headers' ${CORS_HEADERS-'Sec-WebSocket-Extensions,Sec-WebSocket-Key,Sec-WebSocket-Protocol,Sec-WebSocket-Version,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,x-api-action-links,x-api-csrf,x-api-no-challenge,X-Forwarded-For,X-Real-IP'} always;
-        add_header 'Access-Control-Max-Age' 864000 always;
-        add_header 'Content-Type' 'text/plain; charset=UTF-8' always;
-        add_header 'Content-Length' 0 always;
-        return 204;
-      }
+      # if (\$request_method = 'OPTIONS') {
+      #   add_header 'Access-Control-Allow-Origin' $SERVER_NAME always;
+      #   add_header 'Access-Control-Allow-Credentials' \$acac always;
+      #   add_header 'Access-Control-Allow-Methods' ${CORS_METHODS-'GET, POST, PUT, DELETE, HEAD, OPTIONS'} always;
+      #   add_header 'Access-Control-Allow-Headers' ${CORS_HEADERS-'Sec-WebSocket-Extensions,Sec-WebSocket-Key,Sec-WebSocket-Protocol,Sec-WebSocket-Version,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,x-api-action-links,x-api-csrf,x-api-no-challenge,X-Forwarded-For,X-Real-IP'} always;
+      #   add_header 'Access-Control-Max-Age' 864000 always;
+      #   add_header 'Content-Type' 'text/plain; charset=UTF-8' always;
+      #   add_header 'Content-Length' 0 always;
+      #   return 204;
+      # }
 
-      add_header 'Access-Control-Allow-Origin' $SERVER_NAME always;
-      add_header 'Access-Control-Allow-Credentials' \$acac always;
-      add_header 'Access-Control-Allow-Methods' ${CORS_METHODS-'GET, POST, PUT, DELETE, HEAD, OPTIONS'} always;
-      add_header 'Access-Control-Allow-Headers' ${CORS_HEADERS-'Sec-WebSocket-Extensions,Sec-WebSocket-Key,Sec-WebSocket-Protocol,Sec-WebSocket-Version,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,x-api-action-links,x-api-csrf,x-api-no-challenge,X-Forwarded-For,X-Real-IP'} always;
-      add_header 'Access-Control-Max-Age' 864000 always;
+      # add_header 'Access-Control-Allow-Origin' $SERVER_NAME always;
+      # add_header 'Access-Control-Allow-Credentials' \$acac always;
+      # add_header 'Access-Control-Allow-Methods' ${CORS_METHODS-'GET, POST, PUT, DELETE, HEAD, OPTIONS'} always;
+      # add_header 'Access-Control-Allow-Headers' ${CORS_HEADERS-'Sec-WebSocket-Extensions,Sec-WebSocket-Key,Sec-WebSocket-Protocol,Sec-WebSocket-Version,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,x-api-action-links,x-api-csrf,x-api-no-challenge,X-Forwarded-For,X-Real-IP'} always;
+      # add_header 'Access-Control-Max-Age' 864000 always;
 
 EOF
 # Check expires var
